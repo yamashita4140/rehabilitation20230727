cefe4268ac9af2d632415fa03451102cfc46000a
KazzzTen <tengoo1999@gmail.com>
2023-07-27 15:37:54 +0900

    チャレンジ4-3

---
+++ challenge4_3.py
@@ -0,0 +1,14 @@
+# 4章チャレンジ3
+
+def func_bb(x1, x2, x3, y1="Page", y2="Boke"):
+    print(x1, x2, x3, y1, y2)
+
+
+x1 = input("type 1st word:")
+x2 = input("type 2nd word:")
+x3 = input("type 3rd word:")
+y1 = input("type 4th word(op):")
+y2 = input("type 5th word(op):")
+
+func_bb(x1, x2, x3, y1, y2)
+func_bb(x1, x2, x3)
