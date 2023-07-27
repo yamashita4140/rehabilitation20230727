376c3c8cab79dc9f9d1adf883b74fcf6633d75ee
KazzzTen <tengoo1999@gmail.com>
2023-07-27 14:39:45 +0900

    リハビリ三発目。Hello,World10連発

--- shakyou001.py
+++ shakyou001.py
@@ -3,7 +3,10 @@
 import math
 
 # 対角線の長さ
-l = 4
-w = 10
-d = math.sqrt(l**2 + w**2)
-print(d)
+# l = 4
+# w = 10
+# d = math.sqrt(l**2 + w**2)
+# print(d)
+
+for i in range(10):
+    print("Hello,World!!")
