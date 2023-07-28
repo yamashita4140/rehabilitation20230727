b70ddb0ed01868c0e3a0572cf85da72eea4d99ac
KazzzTen <tengoo1999@gmail.com>
2023-07-28 14:09:38 +0900

    第5章写経 メソッド リスト

---
+++ shakyou005.py
@@ -0,0 +1,13 @@
+# 第5章　写経
+
+# メソッド
+print("hello".upper())  # <- 小文字を大文字に
+print("Hello".replace("o", "@"))  # <- 文字の置換
+
+# リスト
+fruit = ["Apple", "Orange", "Pear"]
+print(fruit)
+# リストに要素を追加
+fruit.append("Banana")
+fruit.append("Peach")
+print(fruit)
