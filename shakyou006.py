20f92ed2be1078e557552ae282fd75cfd4f1fd19
KazzzTen <tengoo1999@gmail.com>
2023-07-28 14:19:52 +0900

    第5章写経 リストをいじってみた

---
+++ shakyou006.py
@@ -0,0 +1,13 @@
+# 第5章
+# イテラブル＝繰り返し可能
+# ミュータブル＝変更可能
+
+fruit = ["Apple", "Orange", "Pear"]
+print(fruit[0])
+print(fruit[1])
+print(fruit[2])
+
+colors=["blue","green","yellow"]
+print(colors)
+colors[2]="red"
+print(colors)
