5bad1b3d12c0e16f22165fb941ac62ba2cfb06d1
KazzzTen <tengoo1999@gmail.com>
2023-07-28 14:46:07 +0900

    第5章写経 リストの連結

--- shakyou006.py
+++ shakyou006.py
@@ -7,7 +7,17 @@ print(fruit[0])
 print(fruit[1])
 print(fruit[2])
 
-colors=["blue","green","yellow"]
+colors = ["blue", "green", "yellow"]
 print(colors)
-colors[2]="red"
+colors[2] = "red"
 print(colors)
+
+item = colors.pop()  # popメソッド　リストの最後尾を取り出す
+print(item)
+print(colors)
+
+colors1=["blue","green","yellow"]
+colors2=["orange","pink","black"]
+colors3=colors1+colors2
+print(colors3)
+
