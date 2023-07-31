30fec0874e73e11fd892aa8b736c7fa6e2e7a8b5
KazzzTen <tengoo1999@gmail.com>
2023-07-31 12:08:10 +0900

    第5章チャレンジ 3　辞書を作る　完了

---
+++ challenge5_3.py
@@ -0,0 +1,15 @@
+# 辞書を作る
+
+codes={"1":"Name",
+       "2":"Height",
+       "3":"Weight",
+       "4":"BDay"}
+
+my_data={"Name":"Kazuhisa Yamashita",
+         "Height":"178cm",
+         "Weight":"81Kg",
+         "BDay":"5.15.19XX"
+         }
+
+print(codes)
+print(my_data)
