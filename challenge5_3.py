c9087ba13b5a6c3c2492c00af85be539871962fe
KazzzTen <tengoo1999@gmail.com>
2023-07-31 12:14:25 +0900

    第5章チャレンジ 4　チャレンジ3をいじって入力->出力ができるようにした

--- challenge5_3.py
+++ challenge5_3.py
@@ -1,15 +1,17 @@
 # 辞書を作る
 
-codes={"1":"Name",
-       "2":"Height",
-       "3":"Weight",
-       "4":"BDay"}
+codes = {"1": "Name",
+         "2": "Height",
+         "3": "Weight",
+         "4": "BDay"}
 
-my_data={"Name":"Kazuhisa Yamashita",
-         "Height":"178cm",
-         "Weight":"81Kg",
-         "BDay":"5.15.19XX"
-         }
+my_data = {"Name": "Kazuhisa Yamashita",
+           "Height": "178cm",
+           "Weight": "81Kg",
+           "BDay": "5.15.19XX"
+           }
 
 print(codes)
-print(my_data)
+# print(my_data)
+in_code = input("Enter the number of the information you want to see:")
+print(codes[in_code], ":", my_data[codes[in_code]])
